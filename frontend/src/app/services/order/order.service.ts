343344e6279c9a2e8844366c85aead18088f57d0
sung <nosungju@gmail.com>
2023-01-20 22:40:05 -0300

    refatorando createOrder

--- frontend/src/app/services/order/order.service.ts
+++ frontend/src/app/services/order/order.service.ts
@@ -13,27 +13,30 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
-  public async createOrder(order: Order, orderProduct: OrderProduct[] ): Promise<Order | undefined>{
-    let newOrder:Order | undefined = await firstValueFrom(this.http.post<Order>(`${environment.api}/orders`, order));
-    orderProduct.map(async product=>{
-      let newOrderProduct: OrderProduct | undefined = await firstValueFrom(this.http.post<OrderProduct>(`${environment.api}/orders-products`, {
-        id: 0,
-        order_id: newOrder?.id,
-        product_id: product.product_id,
-        value: product.value,
-        quantity: product.quantity
-      }));
-      let productUpdate:Product | undefined  =  await firstValueFrom(this.http.get<Product>(`${environment.api}/products/${product.product_id}`));
-      let stockUpdate:Product | undefined = await firstValueFrom(this.http.put<Product>(`${environment.api}/products/${product.product_id}`,{
-        id: productUpdate.id,
-        nome: productUpdate.nome,
-        descricao: productUpdate.descricao,
-        valor: productUpdate.valor,
-        qtd_estoque: productUpdate.qtdEstoque - newOrderProduct.quantity
-      }))
-      console.log(stockUpdate)
-    })
-    return newOrder;
+  public async createOrder(order: Order ){
+    // public async createOrder(order: Order, orderProduct: OrderProduct[] ){
+
+    const createO = await firstValueFrom(this.http.post(`${environment.api}pedido`, order))
+    // let newOrder:Order | undefined = await firstValueFrom(this.http.post<Order>(`${environment.api}pedido`, order));
+    // orderProduct.map(async product=>{
+    //   let newOrderProduct: OrderProduct | undefined = await firstValueFrom(this.http.post<OrderProduct>(`${environment.api}pedido`, {
+    //     id: 0,
+    //     order_id: newOrder?.id,
+    //     product_id: product.product_id,
+    //     value: product.value,
+    //     quantity: product.quantity
+    //   }));
+    //   let productUpdate:Product | undefined  =  await firstValueFrom(this.http.get<Product>(`${environment.api}pedido/${product.product_id}`));
+    //   let stockUpdate:Product | undefined = await firstValueFrom(this.http.put<Product>(`${environment.api}pedido/${product.product_id}`,{
+    //     id: productUpdate.id,
+    //     nome: productUpdate.nome,
+    //     descricao: productUpdate.descricao,
+    //     valor: productUpdate.valor,
+    //     qtdEstoque: productUpdate.qtdEstoque - newOrderProduct.quantity
+    //   }))
+    //   console.log(stockUpdate)
+    // })
+    return createO;
   }
 
   public async getOrder(): Promise<Order[] | undefined>{
