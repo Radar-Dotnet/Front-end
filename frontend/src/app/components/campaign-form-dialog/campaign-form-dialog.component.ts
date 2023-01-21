550b26663afcce774e05b508d23a00a3f2fdfffa
sung <nosungju@gmail.com>
2023-01-21 09:18:59 -0300

    trabalhando na campanha

--- frontend/src/app/components/campaign-form-dialog/campaign-form-dialog.component.ts
+++ frontend/src/app/components/campaign-form-dialog/campaign-form-dialog.component.ts
@@ -20,6 +20,10 @@ export class CampaignFormDialogComponent {
 
   faXmark = faXmark;
 
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
 
   closeDialog(): void {
     this.dialogRef.close();
