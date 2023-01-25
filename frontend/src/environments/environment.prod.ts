b7f724ee1548f3e1a1b73499c94e7ea6c7c14626
Moyses Thomaz <moyses.th@gmail.com>
2023-01-25 18:52:09 -0300

    fix: URL prod Api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
     api: "https://dbradar.up.railway.app/api/",
     cepApi:"https://viacep.com.br/ws/",
     estadosApi:"https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-};
+}
