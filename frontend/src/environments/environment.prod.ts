5b8c3afc22419b532452ea0e9893dcf9312b55a0
Moyses Thomaz <moyses.th@gmail.com>
2023-01-25 19:05:28 -0300

    fix: alteração HTTPS p HTTP

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     api: "https://dbradar.up.railway.app/api/",
-    cepApi:"https://viacep.com.br/ws/",
-    estadosApi:"https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+    cepApi:"http://viacep.com.br/ws/",
+    estadosApi:"http://servicodados.ibge.gov.br/api/v1/localidades/estados",
 }
